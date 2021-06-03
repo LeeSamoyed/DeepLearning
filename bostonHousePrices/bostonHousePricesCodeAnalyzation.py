d60e52958db3d1d9e617c017be1a4b09a3f7d672
LeeSamoyed <im.liyi.samoyed@gmail.com>
2021-06-03 13:20:07 +0800

    分解代码

--- bostonHousePrices/bostonHousePricesCodeAnalyzation.py
+++ bostonHousePrices/bostonHousePricesCodeAnalyzation.py
@@ -219,6 +219,7 @@ y = training_data[:, -1:]
 
 ####
 
+# 计算梯度
 # 调用上面定义的gradient函数，计算梯度
 # 初始化网络
 net = Network(13)
@@ -232,3 +233,92 @@ gradient_w5 = gradient_w[5][0]
 gradient_w9 = gradient_w[9][0]
 print('point {}, loss {}'.format([net.w[5][0], net.w[9][0]], loss))
 print('gradient {}'.format([gradient_w5, gradient_w9]))
+
+####
+
+# 确定损失函数更小的点
+# 在[w5, w9]平面上，沿着梯度的反方向移动到下一个点P1
+# 定义移动步长 eta
+eta = 0.1
+# 更新参数w5和w9
+net.w[5] = net.w[5] - eta * gradient_w5
+net.w[9] = net.w[9] - eta * gradient_w9
+# 重新计算z和loss
+z = net.forward(x)
+loss = net.loss(z, y)
+gradient_w, gradient_b = net.gradient(x, y)
+gradient_w5 = gradient_w[5][0]
+gradient_w9 = gradient_w[9][0]
+print('point {}, loss {}'.format([net.w[5][0], net.w[9][0]], loss))
+print('gradient {}'.format([gradient_w5, gradient_w9]))
+
+####
+
+# # Train函数封装
+#
+# class Network(object):
+#     def __init__(self, num_of_weights):
+#         # 随机产生w的初始值
+#         # 为了保持程序每次运行结果的一致性，此处设置固定的随机数种子
+#         np.random.seed(0)
+#         self.w = np.random.randn(num_of_weights, 1)
+#         self.w[5] = -100.
+#         self.w[9] = -100.
+#         self.b = 0.
+#
+#     def forward(self, x):
+#         z = np.dot(x, self.w) + self.b
+#         return z
+#
+#     def loss(self, z, y):
+#         error = z - y
+#         num_samples = error.shape[0]
+#         cost = error * error
+#         cost = np.sum(cost) / num_samples
+#         return cost
+#
+#     def gradient(self, x, y):
+#         z = self.forward(x)
+#         gradient_w = (z - y) * x
+#         gradient_w = np.mean(gradient_w, axis=0)
+#         gradient_w = gradient_w[:, np.newaxis]
+#         gradient_b = (z - y)
+#         gradient_b = np.mean(gradient_b)
+#         return gradient_w, gradient_b
+#
+#     def update(self, gradient_w5, gradient_w9, eta=0.01):
+#         net.w[5] = net.w[5] - eta * gradient_w5
+#         net.w[9] = net.w[9] - eta * gradient_w9
+#
+#     def train(self, x, y, iterations=100, eta=0.01):
+#         points = []
+#         losses = []
+#         for i in range(iterations):
+#             points.append([net.w[5][0], net.w[9][0]])
+#             z = self.forward(x)
+#             L = self.loss(z, y)
+#             gradient_w, gradient_b = self.gradient(x, y)
+#             gradient_w5 = gradient_w[5][0]
+#             gradient_w9 = gradient_w[9][0]
+#             self.update(gradient_w5, gradient_w9, eta)
+#             losses.append(L)
+#             if i % 50 == 0:
+#                 print('iter {}, point {}, loss {}'.format(i, [net.w[5][0], net.w[9][0]], L))
+#         return points, losses
+#
+#
+# # 获取数据
+# train_data, test_data = load_data()
+# x = train_data[:, :-1]
+# y = train_data[:, -1:]
+# # 创建网络
+# net = Network(13)
+# num_iterations = 2000
+# # 启动训练
+# points, losses = net.train(x, y, iterations=num_iterations, eta=0.01)
+# # 画出损失函数的变化趋势
+# plot_x = np.arange(num_iterations)
+# plot_y = np.array(losses)
+# plt.plot(plot_x, plot_y)
+# plt.show()
+
